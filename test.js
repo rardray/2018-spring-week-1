d8cb3d10781b5c682cb9bce9a9d0dedb939b07fb
Ryan Ardray <rardray@gmail.com>
2018-03-03 12:35:55 -0600

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+var name = "ryan";
+var born = 1980
+var year = 2018
+var age = year - born;
+
+console.log("My name is " + name);
+console.log("I am " + age)
+
+if ( age > 40 ){
+    consolde.log ("Damn... you are OLD!!!");
+} else {
+    console.log ("Dang . Yadda YAdda")
+    if ( age == 40) {
+    console.log("soon you will be old as well.");
+} else {
+    console.log("I hate you");
+}
